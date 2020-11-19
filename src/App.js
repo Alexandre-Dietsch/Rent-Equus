c5799cafa2ff301bb286a458438504d4ef2f7879
Louisemehouas <mehouaslouise0@gmail.com>
2020-11-19 19:48:09 +0100

    Horses_display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './styles/app.css'
+import HorsesLibrary from "./HorsesLibrary";
+
 
 export default function App() {
   return (
     <div className="App">
       <h1>Rent Equus</h1>
+      <HorsesLibrary />
     </div>
   );
 }
