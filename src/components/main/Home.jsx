7da0e0962259e783099efa543d2d3e2e684610d6
E-Pasquier <emeric.pasquier@gmail.com>
2020-11-20 14:12:20 +0100

    Finalisation

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../../styles/home.css';
 import { Link } from "react-router-dom";
-import imghome from "../../images/imghome.svg"
+import imghome from "../../images/imghome.png"
 
 export default function Home() {
   return (
     <div className='home-container'>
-      <h1>Need a horse? Rent a horse!</h1>
+      <h1>In need of a horse? Rent one !</h1>
       <Link to="/horse-library">
         <p>Click here to see available horses!</p>
       </Link>
-      <div>
-        <img className="img" src={imghome} alt="home" />
+      <div className="img-home">
+        <img  src={imghome} alt="home" />
       </div>
     </div>
   );
